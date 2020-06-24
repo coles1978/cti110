a689207e8bd980586dfd7e8d553174f5d78fe378
coles1978 <67354315+coles1978@users.noreply.github.com>
2020-06-23 23:22:04 -0400

    Delete P2T1_SalesPrediction_ColeSean.py

--- P2T1_SalesPrediction_ColeSean.py
+++
@@ -1,8 +0,0 @@
-# Sales Profit Calculator
-# 6/22/2020
-# CTI-110 P2T1 - Sales Prediction
-# Sean Cole
-
-total_sales = float(input('Enter the projected sales: '))
-profit = total_sales * 0.23
-print('The profit is $', format(profit, ',.2f'))
